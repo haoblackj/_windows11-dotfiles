4165ea69da82946501eaa1ccee4d9873fc02e528
haoblackj <17177994+haoblackj@users.noreply.github.com>
2022-10-11 17:04:05 +0900

    initial fix

--- setup/install-development-tools.ps1
+++ setup/install-development-tools.ps1
@@ -2,120 +2,19 @@ Write-Host "Install Development Tools"
 Write-Host "=============================="
 
 try {
-  # Scoopのインストール確認
-  get-command scoop -ErrorAction Stop
-} 
+  # chocoのインストール確認
+  get-command choco -ErrorAction Stop
+}
 catch [Exception] {
-  # Scoopのインストール
-  Write-Host "Install Scoop"
-  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+  # chocoのインストール
+  Write-Host "Install choco"
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  choco upgrade chocolatey
 }
 
-# Git
-Write-Host "Install Git"
-scoop install git
-
-# Add bucket
-scoop bucket add extras
-scoop bucket add nonportable
-scoop bucket add nerd-fonts
-# scoop bucket add iyokan-jp https://github.com/tetradice/scoop-iyokan-jp
-
-# Google Chrome
-# Write-Host "Install Google Chrome"
-# scoop install googlechrome
-
-# Firefox
-Write-Host "Install Firefox"
-scoop install firefox
-
-# Slack
-Write-Host "Install Slack"
-scoop install slack
-
-# Discord
-Write-Host "Install Discord"
-scoop install discord
-
-# Dropbox
-Write-Host "Install Dropbox"
-scoop install dropbox-np
-
-# Obsidian
-Write-Host "Install Obsidian"
-scoop install obsidian
-
-# PowerToys
-# Write-Host "Install PowerToys"
-# scoop install powertoys-np
-
-# Windows Terminal
-# Write-Host "Install Windows Terminal"
-# scoop install windows-terminal 
-
-# PowerShell Core
-# Write-Host "Install PowerShell Core"
-# scoop install pwsh
-
-# Visual Studio Code
-Write-Host "Install Visual Studio Code"
-scoop install vscode
-
-# PuTTY
-# Write-Host "Install PuTTY"
-# scoop install PuTTY
-
-# Tera Term
-# Write-Host "Install Tera Term"
-# scoop install teraterm
-
-# Mery
-Write-Host "Install Merry"
-scoop install mery
-
-# Tablacus Explorer
-# Write-Host "Install Tablacus Explorer"
-# scoop install tablacus-explorer
-
-# DeepL
-Write-Host "Install DeepL"
-scoop install deepL
-
-# f.lux
-Write-Host "Install f.lux"
-scoop install flux
-
-# MySQL Workbench
-# Write-Host "Install MySQL Workbench"
-# scoop install mysql-workbench
-
-# Git Kraken
-# Write-Host "Install Git Kraken"
-# scoop install gitkraken
-
-# Postman
-# Write-Host "Install Postman"
-# scoop install postman
-
-# LibreOffice
-Write-Host "Install LibreOffice"
-scoop install libreoffice-stable
-
-# snipaste
-Write-Host "Install snipaste"
-scoop install snipaste
-
-# eartrumpet
-Write-Host "Install eartrumpet"
-scoop install eartrumpet
-
-# zeal
-Write-Host "Install zeal"
-scoop install zeal
-
-# 7zip
-Write-Host "Install 7zip"
-scoop install 7zip
+# 必要なもんゴッソリ
+cinst packages_common.config
+cup all -y
 
 Write-Host ""
 Write-Host "... Install is complete"
