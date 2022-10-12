e4b896c05522ca99549d5bfe78c9f9461844d4d4
haoblackj <17177994+haoblackj@users.noreply.github.com>
2022-10-12 13:26:56 +0900

    Update init.ps1

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/snyt45/windows10-dotfiles/master/setup/windows10-settings.ps1'))
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/snyt45/windows10-dotfiles/master/setup/install-development-tools.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/windows10-dotfiles/master/setup/windows10-settings.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/windows10-dotfiles/master/setup/install-development-tools.ps1'))
 
 Restart-Computer
