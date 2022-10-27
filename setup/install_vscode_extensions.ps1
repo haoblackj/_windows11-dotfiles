37ce8d678fd848903b4de06d618fd17516a528a4
haoblackj <17177994+haoblackj@users.noreply.github.com>
2022-10-27 14:15:38 +0900

    VSCode設定、WSLインストール有無設定追加

---
+++ setup/install_vscode_extensions.ps1
@@ -0,0 +1,4 @@
+$text = Get-Content $args[0]
+foreach ($line in $text) {
+  code.cmd --install-extension $line
+}
--- setup/windows10-settings.ps1
+++ setup/windows10-settings.ps1
@@ -1,8 +1,26 @@
 Write-Host "Windows Settings"
 Write-Host "=============================="
 
-Write-Host "WSL インストール"
-wsl --install
+#選択肢の作成
+$typename = "System.Management.Automation.Host.ChoiceDescription"
+$yes = new-object $typename("&Yes","実行する")
+$no  = new-object $typename("&No","実行しない")
+
+#選択肢コレクションの作成
+$assembly= $yes.getType().AssemblyQualifiedName
+$choice = new-object "System.Collections.ObjectModel.Collection``1[[$assembly]]"
+$choice.add($yes)
+$choice.add($no)
+
+#選択プロンプトの表示
+$answer = $host.ui.PromptForChoice("<実行確認>","実行しますか？",$choice,0)
+
+if($answer -eq 0){
+  Write-Host "WSL インストール"
+  wsl --install
+}Else{
+  Write-Host "WSL インストールしない"
+}
 
 Write-Host ""
 Write-Host "... Setting is complete"
