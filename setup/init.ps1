dbc6ea37a48c5dc48063932c426a7a32c9337c44
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-06-04 14:58:22 +0900

    Update init.ps1

--- setup/init.ps1
+++ setup/init.ps1
@@ -3,5 +3,28 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 reg.exe add "HKCU\Software\Classes\CLSID\{d93ed569-3b3e-4bff-8355-3c44f6a52bb5}\InprocServer32" /f /ve
+winget install -e --id Google.JapaneseIME
+while ($true) {
+    $home = Read-Host -Prompt '自宅端末ですか？ (Yes/No)'
+
+    if ($home -eq 'Yes') {
+        # 設定①
+        Write-Host '自宅の設定を実行します。'
+        winget install -e --id Google.Drive
+        winget install -e --id LINE.LINE
+        winget install -e --id Amazon.Kindle
+        break
+    }
+    elseif ($home -eq 'No') {
+        # 設定②
+        Write-Host '自宅以外の設定を実行します。'
+        # ここに自宅以外の端末用の設定スクリプトを入れてください。
+        break
+    }
+    else {
+        Write-Host '無効な入力です。YesかNoを入力してください。'
+    }
+}
+
 pause
 Restart-Computer
