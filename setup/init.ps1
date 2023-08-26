5f91f2b3679ccc1019f6bad59c76a92483c1741e
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-08-27 08:40:46 +0900

    feat(setup\init.ps1): Google日本語入力は手動でインストールしましょう

--- setup/init.ps1
+++ setup/init.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarAl /t REG_DWORD /d 0 /f
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 reg.exe add "HKCU\Software\Classes\CLSID\{d93ed569-3b3e-4bff-8355-3c44f6a52bb5}\InprocServer32" /f /ve
-winget install -e --id Google.JapaneseIME
+# winget install -e --id Google.JapaneseIME
 while ($true) {
     $home_terminal = Read-Host -Prompt "é©ëÓí[ññÇ≈Ç∑Ç©ÅH (Yes/No)"
 
@@ -13,6 +13,9 @@ while ($true) {
         winget install -e --id Google.Drive
         winget install -e --id LINE.LINE
         winget install -e --id Amazon.Kindle
+        winget install -e --id OBSProject.OBSStudio
+        winget install -e --id Anki.Anki
+        winget install -e --id Mozilla.Firefox
         break
     }
     elseif ($home_terminal -ieq 'No') {
