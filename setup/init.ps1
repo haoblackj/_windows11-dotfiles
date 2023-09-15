fb9b62d647340d14e359c01e7f9a3a545970ee3e
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-09-15 14:10:08 +0900

    fix(setup\init.ps1): 職場PCにも必要なソフトを共通インストール化

--- setup/init.ps1
+++ setup/init.ps1
@@ -2,20 +2,19 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/install-development-tools.ps1'))
 Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 # winget install -e --id Google.JapaneseIME
+winget install -e --id Google.JapaneseIME
+winget install -e --id valinet.ExplorerPatcher
+winget install -e --id Microsoft.Office
 while ($true) {
     $home_terminal = Read-Host -Prompt "自宅端末ですか？ (Yes/No)"
 
     if ($home_terminal -ieq 'Yes') {
         Write-Host "自宅の設定を実行します。"
-        winget install -e --id Google.Drive
         winget install -e --id LINE.LINE
         winget install -e --id Amazon.Kindle
         winget install -e --id OBSProject.OBSStudio
         winget install -e --id Anki.Anki
         winget install -e --id Mozilla.Firefox
-        winget install -e --id Google.JapaneseIME
-        winget install -e --id valinet.ExplorerPatcher
-        winget install -e --id Microsoft.Office
         break
     }
     elseif ($home_terminal -ieq 'No') {
