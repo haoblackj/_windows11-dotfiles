653970a6c5b9277042ea59e0ee9d0ee4b71c21cb
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-09-30 04:18:35 +0900

    feat(setup\init.ps1): Winget構成を導入してみたお

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/windows10-settings.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/install-development-tools.ps1'))
+winget install --id Microsoft.DevHome -e
 Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
 Add-AppxPackage -Path winget.msixbundle
 rm winget.msixbundle
@@ -9,43 +10,5 @@ rm winget.msixbundle
 # winget upgrade --id Microsoft.Winget.Source
 Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -AllUsers
 Get-AppxProvisionedPackage -Online | Where DisplayName -eq "MicrosoftTeams" | Remove-AppxProvisionedPackage -Online
-winget settings --enable InstallerHashOverride
-winget install -e --id Google.JapaneseIME --ignore-security-hash
-winget install -e --id Google.Chrome --ignore-security-hash
-winget install -e --id Bitwarden.Bitwarden --ignore-security-hash
-winget install -e --id voidtools.Everything --ignore-security-hash
-winget install -e --id stnkl.EverythingToolbar --ignore-security-hash
-winget install -e --id SumatraPDF.SumatraPDF --ignore-security-hash
-winget install -e --id valinet.ExplorerPatcher --ignore-security-hash
-winget install -e --id Microsoft.Office --ignore-security-hash
-winget install -e --id Microsoft.WindowsADK --ignore-security-hash
-winget install -e --id AutoHotkey.AutoHotkey --ignore-security-hash
-while ($true) {
-    $home_terminal = Read-Host -Prompt "自宅端末ですか？ (Yes/No)"
-
-    if ($home_terminal -ieq 'Yes') {
-        Write-Host "自宅の設定を実行します。"
-        winget install -e --id AdGuard.AdGuard --ignore-security-hash
-        winget install -e --id File-New-Project.EarTrumpet --ignore-security-hash
-        winget install -e --id CubeSoft.CubePDF --ignore-security-hash
-        winget install -e --id Amazon.Kindle --ignore-security-hash
-        winget install -e --id OBSProject.OBSStudio --ignore-security-hash
-        winget install -e --id Anki.Anki --ignore-security-hash
-        winget install -e --id Mozilla.Firefox --ignore-security-hash
-        winget install -e --id DigitalScholar.Zotero --ignore-security-hash
-        winget install -e --id Synology.ActiveBackupForBusinessAgent --ignore-security-hash
-        winget install -e --id ogdesign.Eagle --ignore-security-hash
-        break
-    }
-    elseif ($home_terminal -ieq 'No') {
-        Write-Host "自宅以外の設定を実行します。"
-        # ここに自宅以外の端末用の設定スクリプトを入れてください。
-        break
-    }
-    else {
-        Write-Host "無効な入力です。YesかNoを入力してください。"
-    }
-}
-
 pause
-Restart-Computer
+# Restart-Computer
