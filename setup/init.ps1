3a46d10feef195c7f409b2c6a07e84d878c5702a
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-10-04 10:07:24 +0900

    feat(setup\init.ps1): よく考えりゃDevHomeは今のところいらないね

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/windows10-settings.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/install-development-tools.ps1'))
-winget install --id Microsoft.DevHome -e
+# winget install --id Microsoft.DevHome -e
 Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
 Add-AppxPackage -Path winget.msixbundle
 rm winget.msixbundle
