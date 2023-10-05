42a8504a87582a828ea009bc8ece3770ee07b0e2
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-10-05 09:05:14 +0900

    feat(setup\init.ps1): WindowsUpdateとストアアプリのアップデートは前提としてやっといてねにした

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,9 +1,20 @@
+# WindowsUpdateを最新まで適用したかどうか、Yes/Noで質問する。Yesならば続行する。
+$boolwindowsUpdate = Read-Host "Is Windows Update up to date? (Yes/No)"
+if ($boolwindowsUpdate -eq "No") {
+    exit
+}
+# Microsoft Storeアプリを最新まで更新したかどうか、Yes/Noで質問する。Yesならば続行する。
+$storeUpdate = Read-Host "Is Microsoft Store up to date? (Yes/No)"
+if ($storeUpdate -eq "No") {
+    exit
+}
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/windows10-settings.ps1'))
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/install-development-tools.ps1'))
 # winget install --id Microsoft.DevHome -e
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
-Add-AppxPackage -Path winget.msixbundle
-rm winget.msixbundle
+# Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle -UseBasicParsing
+# Add-AppxPackage -Path winget.msixbundle
+# rm winget.msixbundle
 # Get-AppxPackage -OutVariable apps | ForEach-Object { Get-AppxPackage -Name $_.Name -OutVariable app; if ($app.SignatureKind -eq 'Store') { Get-AppxPackageUpdate -Name $_.Name | Update-AppxPackage } }
 # Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 # winget install -e --id Google.JapaneseIME
