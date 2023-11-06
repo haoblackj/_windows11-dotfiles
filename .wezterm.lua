8afd5233cb9c7fefc3b9087d4934dd6a68e50dd8
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-11-06 16:59:05 +0900

    Add customizations to WezTerm configuration
    - Add custom color scheme 'Tokyo Night Storm (Gogh)'
    - Set window background opacity to 0.85
    - Configure shortcut keys for toggling full screen, activating copy mode, and pasting from clipboard
    - Set custom font 'PlemolJP HS' with size 16
    - Configure startup settings for WSL (Windows Subsystem for Linux)
    - Configure mouse bindings for right-click paste
    - Create a batch file for deploying WezTerm configuration

---
+++ .wezterm.lua
@@ -0,0 +1,61 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+-- カラースキームの設定
+config.color_scheme = 'Tokyo Night Storm (Gogh)'
+
+-- 背景透過
+config.window_background_opacity = 0.85
+
+-- ショートカットキー設定
+config.keys = {
+  -- Alt+Shift+Fでフルスクリーン切り替え
+  {
+    key = 'F',
+    mods = 'ALT|SHIFT',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  -- Ctrl+Shift+Xでコピーモードの起動
+  {
+    key = 'x',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ActivateCopyMode,
+  },
+  -- Ctrl+Shift+Vでクリップボードからペースト
+  {
+    key = 'v',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.PasteFrom("Clipboard"),
+  },
+}
+
+-- フォントの設定
+config.font = wezterm.font("PlemolJP HS", {weight="Medium", stretch="Normal", style="Normal"})
+
+-- フォントサイズの設定
+config.font_size = 16
+
+-- WSL起動設定
+config.default_prog = {
+  "wsl.exe",
+  "--distribution",
+  "Ubuntu",
+  "--cd",
+  "/home/yagu001",
+  "--exec",
+  "/bin/zsh",
+  "-l"
+}
+
+-- マウスの設定
+config.mouse_bindings = {
+  -- 右クリックでペーストする設定
+  {
+    event={Up={streak=1, button="Right"}},
+    mods="NONE",
+    action=wezterm.action{PasteFrom="Clipboard"}
+  },
+}
+
+return config
