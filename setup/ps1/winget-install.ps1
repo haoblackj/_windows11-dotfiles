5286e3923bcae2f0bde7817d4150bcc002ba064f
haoblackj <17177994+haoblackj@users.noreply.github.com>
2023-12-26 13:41:15 +0900

    Add winget installation script and configuration files

---
+++ setup/ps1/winget-install.ps1
@@ -0,0 +1,18 @@
+New-Item -Path $env:USERPROFILE\.configuration -ItemType Directory
+Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.configuration/configuration.dsc.yaml -OutFile $env:USERPROFILE\.configuration\configuration.dsc.yaml
+Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.configuration/home-configuration.dsc.yaml -OutFile $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
+Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.configuration/home-desktop-configuration.dsc.yaml -OutFile $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
+winget configure $env:USERPROFILE\.configuration\configuration.dsc.yaml
+# 自宅用PCかどうか、Yes/Noで質問する。Yesならば、home-configuration.dsc.yamlを適用する。
+$homeMachine = Read-Host "Is this home PC? (Yes/No)"
+if ($homeMachine -eq "Yes") {
+    winget configure $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
+    # デスクトップPCかどうか、Yes/Noで質問する。Yesならば、home-desktop-configuration.dsc.yamlを適用する。
+    $desktopMachine = Read-Host "Is this home desktop PC? (Yes/No)"
+    if ($desktopMachine -eq "Yes") {
+        winget configure $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
+    }
+}
+#
+
+refreshenv
