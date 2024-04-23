2e2e60ee1c50576a475a0112a2e0080065feccf4
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-04-23 14:29:07 +0900

    feat(setup\init.ps1): エラー処理を追加、再起動することで環境変数を確実に適用するようにした

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,3 +1,22 @@
+#自分自身をダウンロードし、ユーザプロファイルルートに配置する
+#ダウンロードされていたらスキップする
+if (Test-Path "$env:USERPROFILE\init.ps1") {
+    Write-Host "=============================="
+    Write-Host "init.ps1はすでに存在しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "init.ps1をダウンロードしてユーザプロファイルルートに配置します"
+    wget "https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/init.ps1" -OutFile "$env:USERPROFILE\init.ps1"
+    # init.ps1の配置が終わったか確認する
+    if (Test-Path "$env:USERPROFILE\init.ps1") {
+        Write-Host "init.ps1の配置が完了しました"
+    } else {
+        Write-Host "init.ps1の配置が失敗しました"
+    }
+    Write-Host "=============================="
+}
+
 # WindowsUpdateを最新まで適用したかどうか、Yes/Noで質問する。Yesならば続行する。
 $boolwindowsUpdate = Read-Host "Is Windows Update up to date? (Yes/No)"
 if ($boolwindowsUpdate -eq "No") {
@@ -9,28 +28,167 @@ if ($storeUpdate -eq "No") {
     exit
 }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/ps1/wsl-install.ps1'))
+# .gitconfigの配置が終わっているか確認する
+if (Test-Path "$env:USERPROFILE\.gitconfig") {
+    Write-Host "=============================="
+    Write-Host "gitconfigの配置は完了しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    #.gitconfigの配置
+    Write-Host "gitconfigのダウンロードとユーザプロファイルへの配置"
+    wget "https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.gitconfig" -OutFile "$env:USERPROFILE\.gitconfig"
+    # .gitconfigの配置が終わったか確認する
+    if (Test-Path "$env:USERPROFILE\.gitconfig") {
+        Write-Host "gitconfigの配置が完了しました"
+    } else {
+        Write-Host "gitconfigの配置が失敗しました"
+    }
+    Write-Host "=============================="
+}
 
-Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -AllUsers
-Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq "MicrosoftTeams" | Remove-AppxProvisionedPackage -Online
+# Microsoft Teamsがアンインストールされているか確認する
+$teams = Get-AppxPackage -Name MicrosoftTeams
+if ($null -eq $teams) {
+    Write-Host "=============================="
+    Write-Host "Microsoft Teamsはアンインストールされています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "Microsoft Teamsをアンインストールします"
+    Write-Host "=============================="
+    Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -AllUsers
+    Get-AppxProvisionedPackage -Online | Where-Object DisplayName -eq "MicrosoftTeams" | Remove-AppxProvisionedPackage -Online
+}
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/ps1/winget-install.ps1'))
+# WSLのインストールが完了しているか確認する
+# "$env:USERPROFILE\wslInstalled.log"が存在していたら完了と判断する
+# WSLインストールが完了していない場合は、WSLインストールスクリプトを実行する
+if (Test-Path "$env:USERPROFILE\wslInstalled.log") {
+    Write-Host "=============================="
+    Write-Host "WSLのインストールは完了しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "WSLインストールスクリプトを実行します"
+    Write-Host "=============================="
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/ps1/wsl-install.ps1'))
+    Write-Host "=============================="
+    # 5秒待つ
+    Start-Sleep -s 5
+    # OSを再起動する
+    Restart-Computer
+}
 
-mkdir C:\WorkTmp
-git clone https://github.com/kaz399/spzenhan.vim.git C:\WorkTmp\spzenhan.vim
+# ソフトウェアがインストールされたか確認する
+# $env:USERPROFILE\.configurationが存在していたら、インストールされていると判断する
+# インストールされていない場合は、wingetインストールスクリプトを実行する
+if (Test-Path "$env:USERPROFILE\.configuration") {
+    Write-Host "=============================="
+    Write-Host "ソフトウェアはインストールされています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "wingetインストールスクリプトを実行します"
+    Write-Host "=============================="
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/ps1/winget-install.ps1'))
+    Write-Host "=============================="
+    # 5秒待つ
+    Start-Sleep -s 5
+    # OSを再起動する
+    Restart-Computer
+}
 
-git clone https://github.com/haoblackj/_windows11-dotfiles.git C:\WorkTmp\_windows11-dotfiles
+# 作業ディレクトリを作成する
+# 作業ディレクトリが作成されていれば、このフェーズは完了とする
+if (Test-Path "C:\WorkTmp") {
+    Write-Host "=============================="
+    Write-Host "作業ディレクトリは作成されています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "作業ディレクトリを作成します"
+    Write-Host "=============================="
+    New-Item -Path "C:\WorkTmp" -ItemType Directory
+}
 
-# WezTerm_Deploy.batを管理者として実行
-Start-Process C:\WorkTmp\_windows11-dotfiles\WezTerm_Deploy.bat -Verb RunAs
+# Gitが使えるか確認する
+# Gitが使える場合は、下記ふたつのリポジトリをクローンする
+# 1. spzenhan.vim
+# 2. _windows11-dotfiles
+# 使えない場合はエラーメッセージを表示する
+if (Test-Path "C:\Program Files\Git\cmd\git.exe") {
+    Write-Host "=============================="
+    Write-Host "Gitは使えます"
+    Write-Host "=============================="
+    Write-Host "=============================="
+    Write-Host "リポジトリをクローンします"
+    Write-Host "=============================="
+    git clone https://github.com/kaz399/spzenhan.vim.git C:\WorkTmp\spzenhan.vim
+    git clone https://github.com/haoblackj/_windows11-dotfiles.git C:\WorkTmp\_windows11-dotfiles
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "Gitが使えません"
+    Write-Host "=============================="
+}
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/setup/ps1/font-install.ps1'))
+# WezTermの設定を行う
+# WezTermの設定が完了しているか確認する
+# WezTermの設定が完了していない場合は、WezTerm設定スクリプトを実行する
+if (Test-Path "C:\WorkTmp\_windows11-dotfiles\WezTerm_Deploy.bat") {
+    Write-Host "=============================="
+    Write-Host "WezTermの設定は完了しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "WezTerm設定スクリプトを実行します"
+    Write-Host "=============================="
+    Start-Process C:\WorkTmp\_windows11-dotfiles\WezTerm_Deploy.bat -Verb RunAs
+}
 
-gh auth login -w
+# フォントのインストールを行う
+# フォントインストール済のフラグファイルがあるか確認する
+# フォントインストール済のフラグファイルがある場合は、フォントインストール済と判断する
+# フォントインストール済のフラグファイルがない場合は、フォントインストールスクリプトを実行する
+if (Test-Path "$env:USERPROFILE\fontInstalled.log") {
+    Write-Host "=============================="
+    Write-Host "フォントのインストールは完了しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "フォントインストールスクリプトを実行します"
+    Write-Host "=============================="
+    # ローカルにクローンしたフォントインストールスクリプトを実行する
+    # "C:\WorkTmp\_windows11-dotfiles\setup\ps1\font-install.ps1"を実行する
+    # あらためてダウンロードはしない!
+    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression "C:\WorkTmp\_windows11-dotfiles\setup\ps1\font-install.ps1"
+    # フォントインストール済のフラグファイルを作成
+    $fontInstallFlagFile = "$env:USERPROFILE\fontInstalled.log"
+    New-Item -Path $fontInstallFlagFile -ItemType File -Force
+    Write-Host "=============================="
+}
 
-Set-Location C:\WorkTmp
+# GitHub CLIにサインインを行う
+# GitHub CLIにサインインが完了しているか確認する
+# GitHub CLIにサインインが完了していない場合は、サインインを実行する
+if (Test-Path "$env:USERPROFILE\.config\gh\hosts.yml") {
+    Write-Host "=============================="
+    Write-Host "GitHub CLIにサインインは完了しています"
+    Write-Host "=============================="
+} else {
+    Write-Host "=============================="
+    Write-Host "GitHub CLIにサインインを行います"
+    Write-Host "=============================="
+    gh auth login -w
+    Set-Location C:\WorkTmp
+    gh repo clone haoblackj/AutoHotkey
+}
 
-gh repo clone haoblackj/AutoHotkey
+# init.ps1他、作成したフラグファイルを削除する
+Remove-Item -Path "$env:USERPROFILE\init.ps1" -Force
+Remove-Item -Path "$env:USERPROFILE\wslInstalled.log" -Force
+Remove-Item -Path "$env:USERPROFILE\fontInstalled.log" -Force
 
 #pause
 # Restart-Computer
