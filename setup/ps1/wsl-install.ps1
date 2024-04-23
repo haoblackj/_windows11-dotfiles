0c9f3405705d38c714b4a186b049040cced2a33a
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-04-23 14:28:33 +0900

    fix(setup\ps1\wsl-install.ps1): インストール完了フラグファイル作成セクション追加

--- setup/ps1/wsl-install.ps1
+++ setup/ps1/wsl-install.ps1
@@ -1,10 +1,3 @@
-Write-Host "Windows Settings"
-Write-Host "PowerShellの実行ポリシー変更"
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-#.gitconfigの配置
-Write-Host "gitconfigのダウンロードとユーザプロファイルへの配置"
-wget "https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.gitconfig" -OutFile "$env:USERPROFILE\.gitconfig"
-Write-Host "=============================="
 Write-Host "WSLインストールセクション"
 
 #選択肢の作成
@@ -24,12 +17,13 @@ $answer = $host.ui.PromptForChoice("<実行確認>","WSLインストール実行しますか？",
 if($answer -eq 0){
   Write-Host "WSL インストール"
   wsl --install
-  # Start-Sleep -Second 5
-  # Write-Host "Windowsハイパーバイザー インストール"
-  # dism.exe /online /enable-feature /featurename:HypervisorPlatform /all /norestart
 }Else{
   Write-Host "WSL インストールしない"
 }
+
+# WSLインストール完了フラグファイルを作成
+$wslInstallFlagFile = "$env:USERPROFILE\wslInstalled.log"
+New-Item -Path $wslInstallFlagFile -ItemType File -Force
 
 Write-Host ""
 Write-Host "... Setting is complete"
