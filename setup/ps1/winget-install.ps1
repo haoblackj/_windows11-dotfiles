78ecd22f5473ed09399226f28d1d50409bff27f3
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-04-24 15:28:45 +0900

    setup\ps1\winget-install.ps1の変更: homeMachineとdesktopMachineの入力値を大文字小文字を区別せずに判定するように修正

--- setup/ps1/winget-install.ps1
+++ setup/ps1/winget-install.ps1
@@ -5,11 +5,11 @@ Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfile
 winget configure $env:USERPROFILE\.configuration\configuration.dsc.yaml
 # 自宅用PCかどうか、Yes/Noで質問する。Yesならば、home-configuration.dsc.yamlを適用する。
 $homeMachine = Read-Host "Is this home PC? (Yes/No)"
-if ($homeMachine -eq "Yes") {
+if ($homeMachine -ieq "Yes" -or $homeMachine -ieq "y") {
     winget configure $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
     # デスクトップPCかどうか、Yes/Noで質問する。Yesならば、home-desktop-configuration.dsc.yamlを適用する。
     $desktopMachine = Read-Host "Is this home desktop PC? (Yes/No)"
-    if ($desktopMachine -eq "Yes") {
+    if ($desktopMachine -ieq "Yes" -or $desktopMachine -ieq "y") {
         winget configure $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
     }
 }
