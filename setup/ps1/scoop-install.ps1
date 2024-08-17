c12ded72d55e628a85d8016ff41b585b553de036
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-08-17 14:06:28 +0900

    chore: Update scoop-install.ps1 to include git, aria2, 7zip, and bitwarden in the list of installed packages

--- setup/ps1/scoop-install.ps1
+++ setup/ps1/scoop-install.ps1
@@ -1,8 +1,9 @@
 irm get.scoop.sh | iex
 
+scoop install git aria2 7zip
+
 scoop bucket add extras
-scoop bucket add version
+scoop bucket add versions
 scoop bucket add nerd-fonts
 
-scoop install git aria2 7zip
 scoop install sumatrapdf everything powertoys autohotkey vscode wezterm teraterm ueli main/gh googlechrome bitwarden
