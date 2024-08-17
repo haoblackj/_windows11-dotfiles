340ff7a671412d0f3217cc16248337e9e499ca4d
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-08-17 12:23:31 +0900

    chore: Update scoop installation in scoop-install.ps1

---
+++ setup/ps1/scoop-install.ps1
@@ -0,0 +1,7 @@
+irm get.scoop.sh | iex
+
+scoop bucket add extras
+scoop bucket add version
+scoop bucket add nerd-fonts
+
+scoop install git aria2 7zip sumatrapdf everything powertoys autohotkey vscode wezterm teraterm ueli main/gh googlechrome
