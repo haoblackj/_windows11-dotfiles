3789ca3739506b518034e19e0b910f4b364c8f97
haoblackj <17177994+haoblackj@users.noreply.github.com>
2024-12-14 05:36:58 +0000

    wingetインストールスクリプトを整理し、各アプリケーションのインストールコマンドからソース指定を削除

--- setup/ps1/winget-install.ps1
+++ setup/ps1/winget-install.ps1
@@ -4,28 +4,28 @@ Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfile
 Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.configuration/home-configuration.dsc.yaml -OutFile $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
 Invoke-WebRequest https://raw.githubusercontent.com/haoblackj/_windows11-dotfiles/master/.configuration/home-desktop-configuration.dsc.yaml -OutFile $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
 # winget configure $env:USERPROFILE\.configuration\configuration.dsc.yaml
-winget install Google.JapaneseIME --source winget
-winget install Microsoft.PowerShell --source winget
-winget install Biscuit.Biscuit --source winget
-winget install Google.Chrome --source winget
-winget install Mozilla.Firefox --source winget
-winget install Microsoft.Powertoys --source winget --scope machine
-winget install 8bitSolutionsLLC.Bitwarden --source winget
-winget install Bandisoft.Honeyview --source winget
-winget install voidtools.Everything --source winget
-winget install SumatraPDF.SumatraPDF --source winget
-winget install TeraTermProject.teraterm5 --source winget
+winget install Google.JapaneseIME
+winget install Microsoft.PowerShell
+winget install Biscuit.Biscuit
+winget install Google.Chrome
+winget install Mozilla.Firefox
+winget install Microsoft.Powertoys --scope machine
+winget install 8bitSolutionsLLC.Bitwarden
+winget install Bandisoft.Honeyview
+winget install voidtools.Everything
+winget install SumatraPDF.SumatraPDF
+winget install TeraTermProject.teraterm5
 # 自宅用PCかどうか、Yes/Noで質問する。Yesならば、home-configuration.dsc.yamlを適用する。
 $homeMachine = Read-Host "Is this home PC? (Yes/No)"
 if ($homeMachine -ieq "Yes" -or $homeMachine -ieq "y") {
     # winget configure $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
-    winget install AdGuard.AdGuard --source winget
-    winget install CubeSoft.CubePDF --source winget
+    winget install AdGuard.AdGuard
+    winget install CubeSoft.CubePDF
     # デスクトップPCかどうか、Yes/Noで質問する。Yesならば、home-desktop-configuration.dsc.yamlを適用する。
     $desktopMachine = Read-Host "Is this home desktop PC? (Yes/No)"
     if ($desktopMachine -ieq "Yes" -or $desktopMachine -ieq "y") {
         # winget configure $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
-        winget install HermannSchinagl.LinkShellExtension --source winget
+        winget install HermannSchinagl.LinkShellExtension
     }
 }
 # 上記If文が正常に終了したら、フラグファイルを書き出す
