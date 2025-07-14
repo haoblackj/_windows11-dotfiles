f9ee1ba480fc8b14e7d185bc8ded993b1d985609
haoblackj <17177994+haoblackj@users.noreply.github.com>
2025-07-14 22:55:24 +0900

    wingetインストールスクリプトを更新し、qBittorrentのインストールを削除

--- setup/ps1/winget-install.ps1
+++ setup/ps1/winget-install.ps1
@@ -10,23 +10,24 @@ winget install Biscuit.Biscuit
 winget install Google.Chrome
 winget install Mozilla.Firefox
 winget install Microsoft.Powertoys --scope machine
-winget install 8bitSolutionsLLC.Bitwarden
+winget install Bitwarden.Bitwarden
 winget install Bandisoft.Honeyview
 winget install voidtools.Everything
 winget install SumatraPDF.SumatraPDF
-winget install OliverSchwendener.ueli
+# winget install OliverSchwendener.ueli
 # 自宅用PCかどうか、Yes/Noで質問する。Yesならば、home-configuration.dsc.yamlを適用する。
 $homeMachine = Read-Host "Is this home PC? (Yes/No)"
 if ($homeMachine -ieq "Yes" -or $homeMachine -ieq "y") {
     # winget configure $env:USERPROFILE\.configuration\home-configuration.dsc.yaml
     winget install AdGuard.AdGuard
-    winget install CubeSoft.CubePDF
+    # winget install CubeSoft.CubePDF
     # デスクトップPCかどうか、Yes/Noで質問する。Yesならば、home-desktop-configuration.dsc.yamlを適用する。
     $desktopMachine = Read-Host "Is this home desktop PC? (Yes/No)"
     if ($desktopMachine -ieq "Yes" -or $desktopMachine -ieq "y") {
         # winget configure $env:USERPROFILE\.configuration\home-desktop-configuration.dsc.yaml
         winget install HermannSchinagl.LinkShellExtension
         winget install qBittorrent.qBittorrent
+        winget install clawSoft.clawPDF
     }
 }
 # 上記If文が正常に終了したら、フラグファイルを書き出す
